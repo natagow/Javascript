f8239a38dc24ff542aa1f788083292aca3491d77
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-10-19 12:30:03 +0200

    exo 4 petit a fait

---
+++ Javascript/4-BOUCLES/exo 1/exo.js
@@ -0,0 +1,12 @@
+// pair et impair //
+for (var nombre = 0 ; nombre<21; nombre++) {
+  console.log(nombre);
+  if ((nombre%2)==0) {
+          console.log(nombre+" est pair");
+          document.write(nombre+" est pair<br/>");
+      }
+      else {
+          console.log(nombre+" est impair");
+          document.write(nombre+" est impair<br/>");
+      }
+}
---
+++ Javascript/4-BOUCLES/exo.js
@@ -0,0 +1,6 @@
+// pair et impair //
+var i = 0 prompt ("Choisissez votre variable")
+  console.log (i)
+for (var i; i<21; i++){
+  console.log(i);
+}
