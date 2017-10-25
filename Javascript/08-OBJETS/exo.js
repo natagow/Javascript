5f820847197918443afbbc583f710999f8266fa5
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-10-25 17:05:03 +0200

    milieu exo 8

--- Javascript/08-OBJETS/exo.js
+++ Javascript/08-OBJETS/exo.js
@@ -1,10 +1,19 @@
-myCara = {
+// Crée un objet "Character" qui contient les informations suivantes :
+// name (string)
+// age (numero)
+// items_to_give (tableau)
+
+var myCara = {
     name:"Nat",
     age :23,
     items_to_give: ["Lunette", "Epée" , "Tasse"],
+}
 
-  
-    }
-  }
-console.log(myCara);
-console.log(myCara.item);
+
+// afficher chaque information sur une ligne séparés dans la console (for in)
+//
+// faire une fonction "giveItem" qui permet au PNJ de donner un objet aléatoirement
+
+for (var key in myCara) {
+    console.log(key + " : "+ myCara[key]);
+}
