6e57bf3fc19b2189e427f3b24bf3f5131b2ec01b
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-10-28 16:03:50 +0200

    fin exo 8

--- Javascript/08-OBJETS/exo.js
+++ Javascript/08-OBJETS/exo.js
@@ -8,12 +8,62 @@ var myCara = {
     age :23,
     items_to_give: ["Lunette", "Epée" , "Tasse"],
 }
-
+console.log(myCara);
 
 // afficher chaque information sur une ligne séparés dans la console (for in)
-//
+
+for (var a in myCara) {
+console.log(myCara [a]);
+}
+
 // faire une fonction "giveItem" qui permet au PNJ de donner un objet aléatoirement
 
-for (var key in myCara) {
-    console.log(key + " : "+ myCara[key]);
+function giveItem () {
+  var items_to_give = ["café", "chocolat", "aile"]
+  var choix = (Math.floor(Math.random()*items_to_give.length))
+  console.log (items_to_give[choix]);
+}
+
+giveItem ()
+
+var boutique= [
+
+  {
+    title: "BeCodemug",
+    physic: 300,
+    magic: 300,
+    minLevel: 10,
+    available: false,
+  },
+
+  {
+    title: "Souris",
+    physic: 50,
+    magic: 20,
+    minLevel: 2,
+    available: true,
+  },
+
+  {
+    title: "cable",
+    physic: 15,
+    magic: 15,
+    minLevel: 5,
+    available: true,
+  },
+
+  {
+    title: "pc",
+    physic: 250,
+    magic: 25,
+    minLevel: 2,
+    available: false,
+  }
+
+  ];
+
+  function showShop() {
+    for (i in Boutique); {
+  console.log(Boutique[i]);
+}
 }
